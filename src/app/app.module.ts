039d3da299c3e7fe1ac96736af257d86ab4baaea
naimul <naimulhaquesagar24@gmail.com>
2020-05-28 19:55:26 +0600

    page header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2Webstorage } from 'ngx-webstorage';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    RegisterSuccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    Ng2Webstorage.forRoot(),
+    RouterModule.forRoot([
+      {path:'register', component:RegisterComponent},
+      {path:'register-success', component:RegisterSuccessComponent},
+      {path:'login', component:LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
